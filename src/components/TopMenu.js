96394ce32d1b07bdaed2ad880063add9412d23cd
Arranzt <49115180+Arranzt@users.noreply.github.com>
2021-05-11 01:06:53 +0900

    Update TopMenu.js

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -4,7 +4,7 @@ import React from 'react';
 const topElement = (
     <header>
         <ul className="topMenu">
-            <li className="a li">サクラボキ</li>
+            <li className="a li">サクラ</li>
             <li className="b li">ボキ3級</li>
             <li className="c li">ボキ2級</li>
             <li className="d li">ボキ1級</li>
